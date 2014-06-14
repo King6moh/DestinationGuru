c674b7cd8c22a31bbe7cb266c083a512a10addb2
King6moh <king6.moh@gmail.com>
2014-06-14 17:41:58 -0400

    ClientBoundaryTester: 1) Added JUnit test for our UI class

---
+++ src/Client/ClienBoundaryTester.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package Client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Moh
+ *
+ */
+public class ClienBoundaryTester {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
