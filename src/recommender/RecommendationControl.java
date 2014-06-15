1e2b48517c8eefa168dc190e60f920adab661051
Jonathan <bigjon.jf@gmail.com>
2014-06-15 13:28:44 -0400

    Updated ui, working hot or not

--- src/recommender/RecommendationControl.java
+++ src/recommender/RecommendationControl.java
@@ -2,10 +2,27 @@ package recommender;
 
 import dataManagement.AttractionList;
 
+
+
 public class RecommendationControl {
 	
 	private RecommendationEntity recommendation;
 	private AttractionList attractionList;
+	
+	private int counter;
+	
+	public static final String[] attractionListNames = {"Museums",
+	                                                    "Natural Wonders",
+	                                                    "Man Made Wonders",
+	                                                    "Night Clubs",
+	                                                    "Theme Parks",
+	                                                    "Casinos",
+	                                                    "Zoos",
+	                                                    "Cultural",
+	                                                    "Sporting Events",
+	                                                    "Special Events"};
+	public static final int NUM_HOT_OR_NOT = 10;
+	public static final int NUM_HEAD_TO_HEAD = 10;
 	
 	/***
 	 * RecommendationControl constructor
@@ -14,6 +31,19 @@ public class RecommendationControl {
 		
 		recommendation = new RecommendationEntity();
 		attractionList = new AttractionList();
+		counter = 0;
 		
 	} //end constructor
+	
+	public String askHotOrNot(){
+		if(counter < NUM_HOT_OR_NOT){
+			return attractionListNames[counter++];
+		} else {
+			return null;
+		}
+	}
+	
+	public void askHeadToHead(){
+		
+	}
 } //end class
