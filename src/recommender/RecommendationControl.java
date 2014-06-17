be05715a664c280cc4ce1623e45dbb33c75e50d7
King6moh <king6.moh@gmail.com>
2014-06-17 03:49:37 -0400

    Few changes to RecommendationControl and added a tester

--- src/recommender/RecommendationControl.java
+++ src/recommender/RecommendationControl.java
@@ -6,8 +6,6 @@ import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Iterator;
 
-import com.sun.security.ntlm.Client;
-
 import customerBoundary.CustomerBoundary;
 import dataManagement.Attraction;
 import dataManagement.DataControl;
@@ -24,22 +22,6 @@ public class RecommendationControl {
 	private int hnCounter, hhCounter;
 	
 	private String currentTag;
-
-	public static final String[] attractionTypes = {
-		"Museums",
-		"Natural Wonders",
-		"Man Made Wonders",
-		"Night Clubs",
-		"Theme Parks",
-		"Casinos",
-		"Zoos",
-		"Cultural",
-		"Sporting Events",
-	"Special Events"};
-
-	public static final String[] attractionNames = {
-		"The Louvre",
-	"Musee d'Orsay"};
 
 	public static final int NUM_HOT_OR_NOT = 10;
 	public static final int NUM_HEAD_TO_HEAD = 10;
@@ -66,57 +48,6 @@ public class RecommendationControl {
 	{
 		return dataManagement.getList();
 	}
-
-	private ArrayList<String> HotOrNot() {
-
-		Enumeration<String> tagList = recommendation.getTags();
-		String tempTag;
-
-		for (int i = 0; i < NUM_HOT_OR_NOT; i++) {
-
-			while(tagList.hasMoreElements()) {
-				tempTag = tagList.nextElement();
-
-				if (!client.askHotOrNot(tempTag)) {
-					tags.add(tempTag);
-				} //end if
-				else {
-					recommendation.incMatchedTags(tempTag);
-				} //end else
-
-			} //end while
-
-		} //end for loop
-
-		return tags;
-	} //end HotOrNot()
-
-	private void HeadToHead() {
-
-		ArrayList<Attraction> h2h = recommendation.getAttractions();
-		int answer = 0;
-		Attraction attraction_1;
-		Attraction attraction_2;
-
-		for (int i = 0; i < NUM_HEAD_TO_HEAD; i++) {
-
-			while(h2h.iterator().hasNext()) {
-				attraction_1 = h2h.iterator().next();
-				attraction_2 = h2h.iterator().next();
-				answer = client.askHeadToHead(attraction_1, attraction_2);
-				/*
-				if (answer == 1) {
-					h2h.remove(attraction_2);
-				 }
-				else if (answer == 2) {
-					h2h.remove(attraction_1);
-				}
-				 */
-			} //end while
-
-		} //end for loop
-
-	} //end HeadToHead()
 
 	public String askHotOrNot(){
 		if(hnCounter < NUM_HOT_OR_NOT){
